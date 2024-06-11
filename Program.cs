063b82fcd845ee0fc7dc4dd80c0a35bb65ea3997
Alejandro Hurtado <hurtadoalejandro783@gmail.com>
2024-06-11 16:40:34 -0600

    agrega impresion automatica

--- Program.cs
+++ Program.cs
@@ -7,29 +7,32 @@ using FlaUI.UIA3;
 using FlaUI.Core.AutomationElements;
 using FlaUI.Core.Definitions;
 using FlaUI.Core.Conditions;
+using System.Threading.Tasks;
 
 public class Program
 {
+
+    private static bool _try = false;
+
     // Importar funciones de la API de Windows
     [DllImport("user32.dll")]
     private static extern bool SetForegroundWindow(IntPtr hWnd);
 
+    // Importar funciones de la API de Windows
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
     [DllImport("user32.dll", SetLastError = true)]
     private static extern IntPtr FindWindowEx(IntPtr parentHandle, IntPtr childAfter, string className, string windowTitle);
 
+    [DllImport("user32.dll", SetLastError = true)]
+    private static extern int SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+    private const uint WM_CLOSE = 0x0010;
+
+
     public static void Main(string[] args)
     {
-        // Nombre del proceso de Spotify
-        string processName = "Spotify";
-
-        // Adjuntarse a la instancia existente de Spotify
-        var app = AttachToApplication(processName);
-        if (app == null)
-        {
-            Console.WriteLine("Spotify no está abierto.");
-            return;
-        }
-
         // Traer la ventana de Spotify al frente
         FocusSpotifyWindow();
 
@@ -38,62 +41,106 @@ public class Program
             AutomationElement mainPanel = null;
 
             // Intentar obtener el panel de Spotify
-            
-            var allElements = automation.GetDesktop().FindAllChildren(cf => cf.ByControlType(ControlType.Pane));
-            Console.WriteLine($"Se encontraron {allElements.Length} paneles.");
+
+            var allElements = automation.GetDesktop().FindAllChildren(cf => cf.ByControlType(ControlType.Window));
 
             // Buscar el panel de Spotify
             foreach (var element in allElements)
             {
-                Console.WriteLine($"Nombre del elemento: {element.Name}");
-                if (element.Name.Contains("Spotify Premium"))
+                if (element.Name.Contains("Acceso de usuarios"))
                 {
                     mainPanel = element;
-                    break;
                 }
             }
 
 
-            if (mainPanel == null)
+            if (mainPanel != null)
             {
-                Console.WriteLine("No se pudo encontrar el panel principal de Spotify.");
-                return;
+
+                // Esperar a que el panel principal esté disponible
+                mainPanel.WaitUntilClickable();
+
+                var userInput = mainPanel
+                    .FindFirstDescendant(cf => cf.ByControlType(ControlType.Group))
+                    .FindFirstDescendant(panes => panes.ByControlType(ControlType.Pane).And(panes.ByAutomationId("Loginpanel")))
+                    .FindFirstDescendant(cf => cf.ByControlType(ControlType.Edit).And(cf.ByAutomationId("textBoxUsuarios")));
+
+                var passInput = mainPanel
+                    .FindFirstDescendant(cf => cf.ByControlType(ControlType.Group))
+                    .FindFirstDescendant(panes => panes.ByControlType(ControlType.Pane).And(panes.ByAutomationId("Loginpanel")))
+                    .FindFirstDescendant(cf => cf.ByControlType(ControlType.Edit).And(cf.ByAutomationId("textBoxUsuarios")));
+
+                var btnAcceptar = mainPanel
+                    .FindFirstDescendant(cf => cf.ByControlType(ControlType.Group))
+                    .FindFirstDescendant(panes => panes.ByControlType(ControlType.Pane).And(panes.ByAutomationId("Loginpanel")))
+                    .FindFirstDescendant(cf => cf.ByControlType(ControlType.Button).And(cf.ByAutomationId("BtnAceptar")));
+
+
+                //if (userInput != null)
+                //{
+                //    userInput.AsTextBox().Enter("ahurtado");
+                //}
+
+                //if (passInput != null)
+                //{
+                //    passInput.AsTextBox().Enter("ahurtado");
+                //}
+
+                if (btnAcceptar != null)
+                {
+                    btnAcceptar.AsButton().Invoke();
+                }
+            }
+
+            AutomationElement homePanel = null;
+
+            // Buscar el panel de Spotify
+            foreach (var element in automation.GetDesktop().FindAllChildren(cf => cf.ByControlType(ControlType.Window)))
+            {
+                if (element.Name.Contains("Main"))
+                {
+                    homePanel = element;
+                }
             }
 
             // Esperar a que el panel principal esté disponible
-            mainPanel.WaitUntilClickable();
+            homePanel.WaitUntilClickable();
 
-            var playButton = mainPanel
-                .FindFirstDescendant(cf => cf.ByControlType(ControlType.Document))
-                .FindAllDescendants(panes => panes.ByControlType(ControlType.Group));
-
-            var customfilter = mainPanel.FindFirst(TreeScope.Descendants,
-                            new AndCondition(
-                                mainPanel.ConditionFactory.ByControlType(ControlType.Button),
-                                mainPanel.ConditionFactory.ByName("Reproducir")
-                            )
-                        );
+            var auditWindow = homePanel
+                .FindFirstDescendant(cf => cf.ByControlType(ControlType.MenuBar).And(cf.ByAutomationId("menuStrip")))
+                .FindFirstDescendant(cf => cf.ByControlType(ControlType.MenuItem).And(cf.ByName("Audit Activities")))
+                .FindFirstDescendant(cf => cf.ByControlType(ControlType.MenuItem).And(cf.ByName("Audit")));
 
 
+            auditWindow.AsMenuItem().Invoke();
 
-            //.Skip(1)
-            //.Take(1)
-            //.First()  
-            //.FindFirstDescendant(cf => cf.ByControlType(ControlType.Button).And(cf.ByName("Lista 3 Playlist • AlejandroHurtado")));
+            var invoices = homePanel
+                .FindFirstDescendant(cf => cf.ByControlType(ControlType.Window).And(cf.ByAutomationId("FacturasGeneradas")));
 
-            // Navegar en el árbol de elementos para encontrar el botón "Reproducir"
-            //var playButton = mainPanel
-            //    .FindFirstDescendant(cf => cf.ByControlType(ControlType.Document))?
-            //    .FindFirstDescendant(cf => cf.ByControlType(ControlType.Group).And(cf.ByName("Está escuchando:")))?
+            invoices.WaitUntilClickable();
 
-            if (playButton == null)
+            var searchInput = invoices
+                .FindFirstDescendant(cf => cf.ByControlType(ControlType.Pane).And(cf.ByAutomationId("panel1")))
+                .FindFirstDescendant(cf => cf.ByControlType(ControlType.Edit).And(cf.ByAutomationId("textInvoice")));
+
+            var searchBtn = invoices
+                .FindFirstDescendant(cf => cf.ByControlType(ControlType.Pane).And(cf.ByAutomationId("panel1")))
+                .FindFirstDescendant(cf => cf.ByControlType(ControlType.Button).And(cf.ByAutomationId("BtnBuscar")));
+
+            var printBtn = invoices
+                .FindFirstDescendant(cf => cf.ByControlType(ControlType.Pane).And(cf.ByAutomationId("panel2")))
+                .FindFirstDescendant(cf => cf.ByControlType(ControlType.Button).And(cf.ByAutomationId("AsignaInvoice")));
+
+            if (searchInput != null)
             {
-                Console.WriteLine("No se encontró el botón 'Reproducir'.");
-                return;
+                searchInput.AsTextBox().Enter("C08-D1182650");
+
+                if (searchBtn != null) searchBtn.AsButton().Invoke();
+
+                if (printBtn != null) printBtn.AsButton().Invoke();
             }
 
-            // Hacer clic en el botón "Reproducir"
-            customfilter.AsButton().Invoke();
+            Console.ReadLine();
 
             // Esperar un momento para asegurarse de que la canción comienza a reproducirse
             Thread.Sleep(2000);
@@ -103,16 +150,16 @@ public class Program
     private static void FocusSpotifyWindow()
     {
         IntPtr desktopHandle = FindWindowEx(IntPtr.Zero, IntPtr.Zero, "Program", null);
-        IntPtr spotifyHandle = FindWindowEx(desktopHandle, IntPtr.Zero, null, "Spotify Premium");
+        IntPtr spotifyHandle = FindWindowEx(desktopHandle, IntPtr.Zero, "WindowsForms10.Window.8.app.0.141b42a_r9_ad1", null);
 
         if (spotifyHandle != IntPtr.Zero)
         {
             SetForegroundWindow(spotifyHandle);
-            Console.WriteLine("Spotify se ha traído al frente.");
+            Console.WriteLine("App se ha traído al frente.");
         }
         else
         {
-            Console.WriteLine("No se pudo encontrar la ventana de Spotify.");
+            Console.WriteLine("No se pudo encontrar la ventana.");
         }
     }
 
